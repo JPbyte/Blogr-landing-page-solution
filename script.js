a507f3185a6b998a50ec0c6fbdce13147ec10d61
joão pedro fernandes teixeira <pedro.fernandes1058@gmail.com>
2022-11-13 23:56:22 -0300

    main sendo construido

--- script.js
+++ script.js
@@ -7,16 +7,25 @@ const company = document.querySelector(".drop_company")
 const dropThird = document.querySelector(".btn_dropthird")
 const connect = document.querySelector(".drop_connect")
 
+const arrowOne = document.querySelector(".arrow_one")
+const arrowTwo= document.querySelector(".arrow_two")
+const arrowThree = document.querySelector(".arrow_three")
+
 dropFirst.addEventListener("click", () =>{
     down.classList.toggle("show")
+    arrowOne.classList.toggle("rotate")
 })
 
 dropSecund.addEventListener("click", () =>{
     company.classList.toggle("show")
-    
+    arrowTwo.classList.toggle("rotate")
 })
 
 dropThird.addEventListener("click", () =>{
     connect.classList.toggle("show")
-    
+    arrowThree.classList.toggle("rotate")
 })
+
+AOS.init({
+    duration: 1000
+});
