dbf5ba55ab5c24ec4d1edbef5de45bcbaf64ccce
joão pedro fernandes teixeira <pedro.fernandes1058@gmail.com>
2022-11-18 00:00:02 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -16,16 +16,22 @@ const arrowThree = document.querySelector(".arrow_three")
 dropFirst.addEventListener("click", () => {
     down.classList.toggle("show")
     arrowOne.classList.toggle("rotate")
+    company.classList.remove("show")
+    connect.classList.remove("show")
 })
 
 dropSecund.addEventListener("click", () => {
     company.classList.toggle("show")
     arrowTwo.classList.toggle("rotate")
+    down.classList.remove("show")
+    connect.classList.remove("show")
 })
 
 dropThird.addEventListener("click", () => {
     connect.classList.toggle("show")
     arrowThree.classList.toggle("rotate")
+    down.classList.remove("show")
+    company.classList.remove("show")
 })
 
 //variables for dropdown mobile
@@ -47,4 +53,66 @@ closed.addEventListener("click", () => {
     dropDown.style.display = "none"
     closed.style.display = "none"
     menu.style.display = "flex"
+})
+
+//variables
+
+const productDrop = document.querySelector(".product_drop")
+const productArrow = document.querySelector(".product_arrow")
+
+const companyDrop = document.querySelector(".company_drop")
+const companyArrow = document.querySelector(".company_arrow")
+
+const connectDrop = document.querySelector(".connect_drop")
+const connectArrow = document.querySelector(".connect_arrow")
+
+const dropDrop = document.querySelectorAll(".drop_drop")
+
+productDrop.addEventListener("click", () => {
+
+    dropDrop[0].style.display = "flex"
+    dropDrop[1].style.display = "none"
+    dropDrop[2].style.display = "none"
+    productArrow.style.transform = "rotate(180deg)"
+    productArrow.style.transition = "0.4s"
+    companyArrow.style.transform = "rotate(0deg)"
+    companyArrow.style.transition = "0.4s"
+    connectArrow.style.transform = "rotate(0deg)"
+    connectArrow.style.transition = "0.4s"
+})
+companyDrop.addEventListener("click", () => {
+    
+    dropDrop[1].style.display = "flex"
+    dropDrop[0].style.display = "none"
+    dropDrop[2].style.display = "none"
+    companyArrow.style.transform = "rotate(180deg)"
+    companyArrow.style.transition = "0.4s"
+    productArrow.style.transform = "rotate(0deg)"
+    productArrow.style.transition = "0.4s"
+    connectArrow.style.transform = "rotate(0deg)"
+    connectArrow.style.transition = "0.4s"
+})
+
+connectDrop.addEventListener("click", () => {
+    
+    dropDrop[2].style.display = "flex"
+    dropDrop[0].style.display = "none"
+    dropDrop[1].style.display = "none"
+    connectArrow.style.transform = "rotate(180deg)"
+    connectArrow.style.transition = "0.4s"
+    companyArrow.style.transform = "rotate(0deg)"
+    companyArrow.style.transition = "0.4s"
+    productArrow.style.transform = "rotate(0deg)"
+    productArrow.style.transition = "0.4s"
+})
+
+
+
+
+closed.addEventListener("click", () =>{
+    
+    dropDrop[0].style.display = "none"
+    dropDrop[1].style.display = "none"
+    dropDrop[2].style.display = "none"
+
 })
